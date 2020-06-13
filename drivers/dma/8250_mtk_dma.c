68858df9fa33de5fbf1b738521b4faaf09c50762
ZIDAN44 <tuhinsultana075@gmail.com>
2020-06-13 10:39:31 +0200

    drivers: dma: Checkout From begonia-q-oss
    Change-Id: I2e6c79a2d7f294d05459e634f71f5eb5e50e9348
    Signed-off-by: ZIDAN44 <tuhinsultana075@gmail.com>

--- drivers/dma/8250_mtk_dma.c
+++ drivers/dma/8250_mtk_dma.c
@@ -50,8 +50,8 @@
 #define VFF_STOP_CLR_B		0
 #define VFF_FLUSH_B		BIT(0)
 #define VFF_FLUSH_CLR_B		0
-#define VFF_4G_SUPPORT_B	BIT(0)
 #define VFF_4G_SUPPORT_CLR_B	0
+#define VFF_ORI_ADDR_BITS_NUM    32
 
 #define VFF_TX_THRE(n)		((n)*7/8) /* interrupt trigger level for tx */
 #define VFF_RX_THRE(n)		((n)*3/4) /* interrupt trigger level for rx */
@@ -68,7 +68,7 @@ struct mtk_dmadev {
 	struct list_head pending;
 	struct clk *clk;
 	unsigned int dma_requests;
-	bool support_33bits;
+	unsigned int support_bits;
 	unsigned int dma_irq[MTK_SDMA_CHANNELS];
 	struct mtk_chan *lch_map[MTK_SDMA_CHANNELS];
 };
@@ -371,7 +371,7 @@ static void mtk_dma_reset(struct mtk_chan *c)
 	else
 		pr_info("Unknown direction.\n");
 
-	if (mtkd->support_33bits)
+	if (mtkd->support_bits)
 		mtk_dma_chan_write(c, VFF_4G_SUPPORT, VFF_4G_SUPPORT_CLR_B);
 }
 
@@ -499,11 +499,6 @@ static void mtk_dma_free_chan_resources(struct dma_chan *chan)
 		free_irq(mtkd->dma_irq[c->dma_ch], chan);
 	}
 
-	/* disable the tasklet. if the task is running, wait for it finish and
-	 * remove it from tasklet list. Put it before reset channel paremeter
-	 * because it may running.
-	 */
-	tasklet_kill(&mtkd->task);
 	c->channel_base = NULL;
 	mtkd->lch_map[c->dma_ch] = NULL;
 	vchan_free_chan_resources(&c->vc);
@@ -511,6 +506,7 @@ static void mtk_dma_free_chan_resources(struct dma_chan *chan)
 	pr_debug("freeing channel for %u\n", c->dma_sig);
 	c->dma_sig = 0;
 
+	tasklet_kill(&mtkd->task);
 	pm_runtime_put_sync(mtkd->ddev.dev);
 }
 
@@ -695,6 +691,10 @@ static int mtk_dma_slave_config(struct dma_chan *chan,
 		mtk_dma_chan_write(c, VFF_INT_FLAG, VFF_RX_INT_FLAG_CLR_B);
 		mtk_dma_chan_write(c, VFF_EN, VFF_EN_B);
 
+		if (mtkd->support_bits > VFF_ORI_ADDR_BITS_NUM)
+			mtk_dma_chan_write(c, VFF_4G_SUPPORT,
+					upper_32_bits(cfg->src_addr));
+
 		if (c->requested == false) {
 			atomic_set(&c->entry, 0);
 			c->requested = true;
@@ -715,6 +715,10 @@ static int mtk_dma_slave_config(struct dma_chan *chan,
 		mtk_dma_chan_write(c, VFF_INT_FLAG, VFF_TX_INT_FLAG_CLR_B);
 		mtk_dma_chan_write(c, VFF_EN, VFF_EN_B);
 
+		if (mtkd->support_bits > VFF_ORI_ADDR_BITS_NUM)
+			mtk_dma_chan_write(c, VFF_4G_SUPPORT,
+					upper_32_bits(cfg->dst_addr));
+
 		if (c->requested == false) {
 			c->requested = true;
 			ret = request_irq(mtkd->dma_irq[c->dma_ch],
@@ -727,9 +731,6 @@ static int mtk_dma_slave_config(struct dma_chan *chan,
 		}
 	} else
 		pr_info("Unknown direction!\n");
-
-	if (mtkd->support_33bits)
-		mtk_dma_chan_write(c, VFF_4G_SUPPORT, VFF_4G_SUPPORT_B);
 
 	if (mtk_dma_chan_read(c, VFF_EN) != VFF_EN_B) {
 		pr_err("config dir%d dma fail\n", cfg->direction);
@@ -825,6 +826,7 @@ static int mtk_dma_probe(struct platform_device *pdev)
 	struct mtk_dmadev *mtkd;
 	struct resource *res;
 	int rc, i;
+	unsigned int addr_bits = VFF_ORI_ADDR_BITS_NUM;
 
 	mtkd = devm_kzalloc(&pdev->dev, sizeof(*mtkd), GFP_KERNEL);
 	if (mtkd == NULL)
@@ -854,15 +856,13 @@ static int mtk_dma_probe(struct platform_device *pdev)
 		return PTR_ERR(mtkd->clk);
 	}
 
-	if (of_property_read_bool(pdev->dev.of_node, "dma-33bits")) {
-		pr_info("Support dma 33bits\n");
-		mtkd->support_33bits = true;
-	}
+	if (of_property_read_u32(pdev->dev.of_node, "dma-bits", &addr_bits))
+		addr_bits = VFF_ORI_ADDR_BITS_NUM;
 
-	if (mtkd->support_33bits)
-		rc = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(33));
-	else
-		rc = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32));
+	pr_info("DMA address bits: %d\n", addr_bits);
+
+	mtkd->support_bits = addr_bits;
+	rc = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(addr_bits));
 	if (rc)
 		return rc;
 
