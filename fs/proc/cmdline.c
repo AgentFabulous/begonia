89a541073cc7716913f7e05c6d2f7af0738ba59f
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-01 18:56:59 +0530

    cmdline: Append begonia SKU to cmdline as needed
    Change-Id: I7196417c7c77a8f0ca63fe8c835632c41cceca74

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,10 +3,20 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/slab.h>
+#include <asm/setup.h>
+
+#ifdef CONFIG_PROC_BEGONIA_CMDLINE
+static char patched_cmdline[COMMAND_LINE_SIZE];
+#endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifndef CONFIG_PROC_BEGONIA_CMDLINE
 	seq_printf(m, "%s\n", saved_command_line);
+#else
+	seq_printf(m, "%s\n", patched_cmdline);
+#endif
 	return 0;
 }
 
@@ -22,8 +32,49 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+#ifdef CONFIG_PROC_BEGONIA_CMDLINE
+static void append_cmdline(char *cmd, const char *flag_val) {
+	strncat(cmd, " ", 2);
+	strncat(cmd, flag_val, strlen(cmd) + 1);
+}
+
+static bool check_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t f_len, r_len, v_len;
+	char *f_pos, *v_pos, *v_end;
+	char *r_val;
+
+	f_pos = strstr(cmd, flag);
+	if (!f_pos) {
+		return false;
+	}
+	f_len = strlen(flag);
+	v_len = strlen(val);
+	v_pos = f_pos + f_len;
+	v_end = v_pos + strcspn(f_pos + f_len, " ");
+	r_len = v_end - v_pos;
+	if ((r_val = kmalloc(r_len + 1, GFP_KERNEL)) == NULL)
+		return false;
+	memcpy(r_val, v_pos, r_len + 1);
+	bool ret = r_len == v_len && !memcmp(r_val, val, r_len);
+	return ret;
+}
+
+static void patch_begonia_cmdline(char *cmdline)
+{
+	if(!check_flag(cmdline, "androidboot.hwc=", "India"))
+		append_cmdline(cmdline, "androidboot.product.hardware.sku=begonia");
+}
+#endif
+
 static int __init proc_cmdline_init(void)
 {
+#ifdef CONFIG_PROC_BEGONIA_CMDLINE
+	strcpy(patched_cmdline, saved_command_line);
+
+	patch_begonia_cmdline(patched_cmdline);
+#endif
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
