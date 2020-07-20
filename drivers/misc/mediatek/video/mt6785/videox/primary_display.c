1c9b8a27ce8b57eda37c61d0422955c9925b5c7e
ZyCromerZ <neetroid97@gmail.com>
2020-07-20 19:14:24 +0700

    drivers: mediatek-video: primary_display: add module to get current display framerate * tested and working well xD Note :   - display_framerate_main : to get main display framerate   - display_framerate_ext  : to get ext display framerate
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- drivers/misc/mediatek/video/mt6785/videox/primary_display.c
+++ drivers/misc/mediatek/video/mt6785/videox/primary_display.c
@@ -148,6 +148,9 @@ static int osc_last_stat;
 static bool has_rsz_input;
 static bool has_yuv_input;
 
+static uint display_framerate_main;
+static uint display_framerate_ext;
+
 #if 0
 /* global variable for idle manager */
 static unsigned long long idlemgr_last_kick_time = ~(0ULL);
@@ -847,6 +850,7 @@ static unsigned int _fps_ctx_get_avg_fps(struct fps_ctx_t *fps_ctx)
 	if (fps_ctx->cur_wnd_sz == 0)
 		return 0;
 	avg_fps = fps_ctx->total / fps_ctx->cur_wnd_sz;
+	display_framerate_main = avg_fps;
 	return avg_fps;
 }
 
@@ -856,6 +860,7 @@ static unsigned int _fps_ctx_get_avg_fps_ext(struct fps_ctx_t *fps_ctx,
 	unsigned int avg_fps;
 
 	avg_fps = (fps_ctx->total + abs_fps) / (fps_ctx->cur_wnd_sz + 1);
+	display_framerate_ext = avg_fps;
 	return avg_fps;
 }
 
@@ -11405,3 +11410,5 @@ int primary_display_set_panel_param(unsigned int param)
 	return ret;
 }
 
+module_param(display_framerate_main, uint, 0664);
+module_param(display_framerate_ext, uint, 0664);
