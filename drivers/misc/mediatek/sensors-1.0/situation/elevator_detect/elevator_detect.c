bb856f0cd8b0d1cf4942d74325f6280d09d4943e
SahilSonar <sss.sonar2003@gmail.com>
2021-07-05 08:59:23 +0200

    drivers: sensors-1.0: Fix misleading indentations Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- drivers/misc/mediatek/sensors-1.0/situation/elevator_detect/elevator_detect.c
+++ drivers/misc/mediatek/sensors-1.0/situation/elevator_detect/elevator_detect.c
@@ -83,7 +83,7 @@ static int elevator_detect_recv_data(struct data_unit_t *event, void *reserved)
 	else if (event->flush_action == DATA_ACTION)
 		//err = situation_notify_t(ID_ELEVATOR_DETECT , (int64_t)event->time_stamp);
 		value[0] = event->elevator_data_t.status;
-		err = elevator_data_report_t(value, (int64_t)event->time_stamp);
+	err = elevator_data_report_t(value, (int64_t)event->time_stamp);
 	return err;
 }
 
